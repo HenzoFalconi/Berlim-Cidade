542dbfcb32cc59a0edddeed7e936a112f9735828
Henzo Falconi <159260627+HenzoFalconi@users.noreply.github.com>
2024-07-31 09:28:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let legendas = document.getElementById ("legenda")
+let imagens = document.getElementById ("imagem")
+function Imagem1 ()
+{
+    legendas.innerHTML="Capital"
+    imagens.src = "Imagens/BerlimCapital.jpeg"
+}
+
+function Imagem2 ()
+{
+    legendas.innerHTML="Catedral"
+    imagens.src = "Imagens/BerlimCatedral.jpg"
+}
+
+function Imagem3 ()
+{
+    legendas.innerHTML="Estatua"
+    imagens.src = "Imagens/BerlimEstatua.jpg"
+}
+
+function Imagem4 ()
+{
+    legendas.innerHTML="Panaroma"
+    imagens.src = "Imagens/BerlimPanaroma.jpg"
+}
